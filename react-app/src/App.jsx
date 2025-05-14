3ad33d2f66de1e9e95811564986972ec902aa9ef
tmeth <methtoby@gmail.com>
2025-05-14 18:59:34 -0400

    add navBar, header, footer to app.jsx

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,15 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Details from './Details';
+import AboutUs from './AboutUs';
+import Navbar from './navBar';
+import Footer from './footer';
+import Header from './header';
 import './App.css';
 
 export default function App() {
   return (
     <Router>
+        <Navbar /> {/* Navbar at the top */}
+        <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/details/:id" element={<Details />} />
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
+        <Footer /> {/* Footer appears at the bottom */}
     </Router>
   );
 }
