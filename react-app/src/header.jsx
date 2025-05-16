c3af49d6f7ebddd969282cd5869f13ff51f0acff
tmeth <methtoby@gmail.com>
2025-05-16 00:05:46 -0400

    changing colors

--- react-app/src/header.jsx
+++ react-app/src/header.jsx
@@ -3,7 +3,7 @@ import imgStyle from './Image.module.css'
 function Header() {
     return (
       <header className={styles.header}>
-        <img src="logo.ico" alt="Logo" className={imgStyle.logo}/>
+        <img src="Logo.png" alt="Logo" className={imgStyle.logo}/>
         {/* <h1>Find-Me-A-Recipe</h1> */}
       </header>
     );
