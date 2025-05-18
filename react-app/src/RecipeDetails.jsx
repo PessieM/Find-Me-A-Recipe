7e5dbdd18e86ca1d29dddbd51c1d7d55b2ea8176
Pesia Mittelman <pessie.mittelman@gmail.com>
2025-05-18 10:35:42 -0400

    fixing backbutton on details page

--- react-app/src/RecipeDetails.jsx
+++ react-app/src/RecipeDetails.jsx
@@ -1,9 +1,9 @@
 export default function RecipeDetails({ title, instructions, image, ingredients }) {
     return (
         <div>
-            <h2>{title}</h2>
+            <h1>{title}</h1>
             <img src={image} alt={title} width="30%"/>
-            <h3>Ingredients</h3>
+            <h2>Ingredients</h2>
             <ul>
                 {ingredients.map((item, index) => (
                     <li key={index}>
@@ -11,14 +11,9 @@ export default function RecipeDetails({ title, instructions, image, ingredients 
                     </li>
                 ))}
             </ul>
-            <h3>Instructions</h3>
-            {/* one paragraph of instructions */}
+            <h2>Instructions</h2>
             <p>{instructions}</p>
-            {/* or each sentence on a new line */}
-            {/* <p>{instructions.split('.').map((sentence, index) => (
-                    sentence.trim() && <span key={index}>{sentence.trim()}.<br /></span>
-            ))}
-            </p> */}
+            <BackButton />
         </div>
     );
 }
